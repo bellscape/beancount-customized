bcc730624142dd30fa2995310566406af610d856
bell <bell>
2024-02-25 23:07:29 +0000

    web: init

---
+++ web/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{ts,tsx,css,less}', 'index.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 5001,
+        strictPort: true,
+        open: true,
+        proxy: {
+            '/ws': {
+                target: 'http://127.0.0.1:5002',
+                changeOrigin: true,
+            },
+        },
+    },
+})
